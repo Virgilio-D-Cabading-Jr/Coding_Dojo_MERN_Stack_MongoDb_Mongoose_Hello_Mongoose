8b6243578b0dbae81f13ce52b4439c3fde2d36e3
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-21 15:25:31 -0800

    configure user.models.js

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,10 +1,15 @@
+// //// Import Mongoose ////////
 const mongoose = require("mongoose");
 
+// //// SCHEMA and Rulse ////////
 const UserSchema = new mongoose.Schema({
 	name: String,
 	age: Number
 });
 
+// //// MODEL ////////
+// This is what we use to make the actual queries to the db
 const User = mongoose.model("User", UserSchema);
 
+// //// Export the Model ////////
 module.exports = User;
