7d678fcb561f9f2aa0181a7930676a345b83f166
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-21 15:40:29 -0800

    add to user.controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,16 +1,11 @@
+// Import Model into the Controller
 const User = require("../models/user.model");
 
-module.exports.findAllUsers = (req, res) => {
-  User.find()
-    .then(allDaUsers => res.json({ users: allDaUsers }))
-    .catch(err => res.json({ message: "Something went wrong", error: err }));
-};
+//////////////////////////////////////////
+//  USER CONTROLLER
+//////////////////////////////////////////
 
-module.exports.findOneSingleUser = (req, res) => {
-	User.findOne({ _id: req.params.id })
-		.then(oneSingleUser => res.json({ user: oneSingleUser }))
-		.catch(err => res.json({ message: "Something went wrong", error: err }));
-};
+// //// CREATE ///////////////////////////
 
 module.exports.createNewUser = (req, res) => {
   User.create(req.body)
@@ -18,14 +13,48 @@ module.exports.createNewUser = (req, res) => {
     .catch(err => res.json({ message: "Something went wrong", error: err }));
 };
 
+// //// RETRIEVE /////////////////////////
+
+/**
+ * **** Find All Users ********
+ * @param {*} req 
+ * @param {*} res 
+ */
+module.exports.findAllUsers = (req, res) => {
+  // db.Users.find()
+  User.find()
+    // Important: what we return here is that we receive in React
+    .then(allDaUsers => res.json({ users: allDaUsers }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
+};
+
+/**
+ * **** Find ONE User by ID ********
+ * @param {*} req 
+ * @param {*} res 
+ */
+// User.findByID(req.params.id) <-- Alternate way of doing next line
+module.exports.findOneSingleUser = (req, res) => {
+	User.findOne({ _id: req.params.id })
+		.then(oneSingleUser => res.json({ user: oneSingleUser }))
+		.catch(err => res.json({ message: "Something went wrong", error: err }));
+};
+
+// //// UPDATE ////////////////////////////
+
 module.exports.updateExistingUser = (req, res) => {
   User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
     .then(updatedUser => res.json({ user: updatedUser }))
     .catch(err => res.json({ message: "Something went wrong", error: err }));
 };
 
+// //// DELETE ////////////////////////////
+
 module.exports.deleteAnExistingUser = (req, res) => {
   User.deleteOne({ _id: req.params.id })
     .then(result => res.json({ result: result }))
     .catch(err => res.json({ message: "Something went wrong", error: err }));
 };
+
+
+
